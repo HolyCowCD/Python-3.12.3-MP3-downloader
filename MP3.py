0fe878d17a066c74616ade1ead7ea8caee3652a3
Holy Cow <ultrarc109@gmail.com>
2024-07-29 23:43:10 +0100

    Update MP3.py
    changed to yt-dlp cus its better ig
    pytube broke :(

--- MP3.py
+++ MP3.py
@@ -1,5 +1,5 @@
 from colorama import Style, Fore, init  # for color
-from pytube import YouTube  # for downloading from YouTube
+from yt_dlp import YoutubeDL  # for downloading from YouTube
 from mutagen.id3 import ID3, ID3NoHeaderError, TIT2, TPE1, TALB, APIC  # for metadata
 from mutagen.mp3 import MP3
 import traceback  # for error handling
@@ -7,54 +7,68 @@ from moviepy.editor import VideoFileClip  # for conversion
 import os
 import requests  # for downloading the thumbnail
 
-init(autoreset=True)# Initialize colorama or whatever
-save = r"Mp3Downloads"  # this is where the files get saved
+init(autoreset=True)  # Initialize colorama
+save = r"c:\Users\uujja\Downloads"  # this is where the files get saved
 
-if not os.path.exists(save):# makes path if 'save' folder is not found
-    os.makedirs(save)
-    
-def grint(text): 
+def grint(text):
     print(Fore.GREEN + text + Style.RESET_ALL)
-# these are for styling 
+
 def print_info(label, info):
     print(Fore.CYAN + label + ": " + Style.BRIGHT + Fore.WHITE + str(info) + Style.RESET_ALL)
+
+def download_video(url, output_path):
+    ydl_opts = {
+        'format': 'best',
+        'outtmpl': os.path.join(output_path, '%(title)s.%(ext)s'), # no clue what this does ask gpt lol
+        'quiet': True # same with this line lol
+    }
+    with YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(url, download=True)
+    return info_dict
 
 while True:  # Loops until the program is shut
     try:
         yt_link = input(Fore.WHITE + "Enter YouTube link: " + Style.RESET_ALL)
-        yt = YouTube(yt_link)
-        print_info("Title", yt.title)
-        print_info("Author", yt.author)
-        print_info("Views", yt.views)
-        print_info("Length", yt.length)
+        video_info = download_video(yt_link, save)
+        
+        # just here incase i need to use later
+        title = video_info.get('title')
+        author = video_info.get('uploader')
+        views = video_info.get('view_count')
+        length = video_info.get('duration')
+        thumbnail_url = video_info.get('thumbnail')
+        mp4_path = os.path.join(save, f"{title}.mp4")
 
-        yd = yt.streams.get_highest_resolution()  # best quality for mp4
-        mp4_path = yd.download(output_path=save)
+        print_info("Title", title)
+        print_info("Author", author)
+        print_info("Views", views)
+        print_info("Length", length)
+
         grint("Mp4 downloaded with no metadata!")
 
         # Converting to mp3
-        mp3_filename = yt.title + ".mp3"  # for renaming it
-        mp3_path = os.path.join(save, mp3_filename)  # download a copy at 'save' as a .mp3
-        video_clip = VideoFileClip(mp4_path)  # this is to make a variable
-        video_clip.audio.write_audiofile(mp3_path)  # actually downloads
-        video_clip.close()  # stops the conversion
+        mp3_filename = f"{title}.mp3"
+        mp3_path = os.path.join(save, mp3_filename)
+        video_clip = VideoFileClip(mp4_path)
+        video_clip.audio.write_audiofile(mp3_path)
+        video_clip.close()
         grint("MP3 ready for metadata :)")
 
         # Adding metadata
-        try:  # using the advanced format (more verstile i think)
+        try:
             audio = ID3(mp3_path)
         except ID3NoHeaderError:
             audio = ID3()
 
-        audio.add(TIT2(encoding=3, text=yt.title))  # Title
-        audio.add(TPE1(encoding=3, text=yt.author))  # Artist
-        audio.add(TALB(encoding=3, text=yt.title))  # Album
+        audio.add(TIT2(encoding=3, text=title))  # Title
+        audio.add(TPE1(encoding=3, text=author))  # Artist
+        audio.add(TALB(encoding=3, text=title))  # Album
 
-        audio.save() # save the tags?
+        audio.save()
 
         # Download the thumbnail image
-        thumbnail_path = os.path.join(save, "thumbnail.png")  # empty image file
-        response = requests.get(yt.thumbnail_url)  # writes to empty image file
+        thumbnail_path = os.path.join(save, "thumbnail")  
+        response = requests.get(thumbnail_url)
         with open(thumbnail_path, 'wb') as file:
             file.write(response.content)
 
@@ -63,26 +77,25 @@ while True:  # Loops until the program is shut
         with open(thumbnail_path, 'rb') as img_file:
             audio.tags.add(
                 APIC(
-                    encoding=3,  # utf-8 cus 2^3 = 8
-                    mime="image/png",  # uses png, might be an issue if someone else is a png
-                    type=3,  # 3 = cover image :)
+                    encoding=3,
+                    mime="image/jpeg",  # Changed to image/jpeg for .jpg
+                    type=3,
                     desc='Cover',
-                    data=img_file.read() 
+                    data=img_file.read()
                 )
             )
 
         # Save the audio with the new tags
-        audio.save(v2_version=3)  # saving in ID3v2.3 format, whatever that means 
+        audio.save(v2_version=3)
         grint("Metadata and thumbnail added :)")
         grint("MP3 Downloaded!")
 
         # Clean up
-        os.remove(thumbnail_path)  # remove the downloaded thumbnail file
-        os.remove(mp4_path)  # remove the downloaded mp4 file
+        os.remove(mp4_path)
         grint("Clean up finished")
 
     except Exception as e:
-        print(Fore.RED + f"Something went wrong!: {e}")  # not very good :(
-        print(traceback.format_exc() + Style.RESET_ALL)  # this gives more info
-
+        print(Fore.RED + f"Something went wrong!: {e}")
+        print(traceback.format_exc() + Style.RESET_ALL)
+        input("Press enter to continue!")
     os.system('cls')
